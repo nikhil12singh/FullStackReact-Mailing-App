173a6f3e79baf9f8eb73c7f863f5a82adfe9fb4f
nikhil <nksingh2@asu.edu>
2018-07-07 21:40:54 -0700

    changed

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req,res) => {
-	res.send({hi: 'there'});
+	res.send({bye: 'buddy'});
 });
 
-const PORT = process.env.port || 5000
-app.listen(PORT);
+app.listen(process.env.port || 5000);
