39f9eb6546268c6ca0c3dbe9ef5e73d969ab80b6
nikhil <nksingh2@asu.edu>
2018-07-08 21:15:31 -0700

    Full stack react + node application which provides mailing services

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+require('./service/passport');
 
+/*
 app.get('/', (req,res) => {
 	res.send({bye: 'buddy'});
 });
+*/
 
-app.listen(process.env.port || 5000);
+// 802514765834-0umqvlfbci87s8aumngers4v94lck7l2.apps.googleusercontent.com
+// KGAcS2VUJ-awqRs-Hj0KjzAu
+require('./routes/authRoutes')(app);
+const PORT = process.env.port;
+app.listen(PORT || 5000);
